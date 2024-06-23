0b999d85e6139618d83b8fc7622788686931a4cb
Robert Galluccio <robertnw5@gmail.com>
2024-06-23 14:56:50 -0400

    Allow custom asset input

--- DemoPlayer/ContentView.swift
+++ DemoPlayer/ContentView.swift
@@ -1,14 +1,33 @@
 import SwiftUI
 
-let demoAssetURL = URL(
-    string: "https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_adv_example_hevc/master.m3u8"
-)!
+let defaultUrl = "https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_adv_example_hevc/master.m3u8"
 
 struct ContentView: View {
+    @State var assetUrl = ""
+    private var playbackUrl: String { assetUrl.isEmpty ? defaultUrl : assetUrl }
+
     var body: some View {
         NavigationStack {
+            TextField(text: $assetUrl) {
+                Text("Asset URL")
+            }
+            .autocorrectionDisabled(true)
+            .textInputAutocapitalization(.never)
+            .textContentType(.URL)
+            .textFieldStyle(.roundedBorder)
+            .padding([.top, .horizontal])
+
+            Text("If no URL is provided then a default asset is used.")
+                .font(.footnote)
+                .fontWeight(.light)
+                .padding(.bottom)
+
             NavigationLink {
-                PlayerPageView(assetURL: demoAssetURL)
+                if let url = URL(string: playbackUrl) {
+                    PlayerPageView(assetURL: url)
+                } else {
+                    Text("Invalid URL: \(assetUrl)")
+                }
             } label: {
                 Label("Play", systemImage: "play")
             }
