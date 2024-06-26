2c47a3e9ab4c011495de46ff543e65562a69422e
Robert Galluccio <robertnw5@gmail.com>
2024-06-25 21:24:38 -0400

    Added comment for other Apple test stream I keep using

--- DemoPlayer/DemoPlayerApp.swift
+++ DemoPlayer/DemoPlayerApp.swift
@@ -2,6 +2,7 @@ import AVFoundation
 import SwiftUI
 
 // Constants
+//let defaultUrl = "https://devstreaming-cdn.apple.com/videos/streaming/examples/adv_dv_atmos/main.m3u8"
 let defaultUrl = "https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_adv_example_hevc/master.m3u8"
 let playerUsesCustomInfoViewControllers: Bool = {
     #if os(iOS)
